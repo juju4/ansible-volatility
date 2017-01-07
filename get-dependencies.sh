493eeaf150e0e7e26faf94d17c30285259de2168
juju4 <juju4@users.noreply.github.com>
2017-01-07 07:43:40 -0500

    sync repositories - add Jenkinsfile, galaxy naming, scan optional task

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -3,12 +3,17 @@
 
 umask 022
 
+if [ $# != 0 ]; then
+rolesdir=$1
+else
 rolesdir=$(dirname $0)/..
+fi
 
-[ ! -d $rolesdir/juju4.maxmind ] && git clone https://github.com/juju4/ansible-maxmind $rolesdir/juju4.maxmind
-[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
-[ ! -d $rolesdir/kbrebanov.selinux ] && git clone https://github.com/kbrebanov/ansible-selinux.git $rolesdir/kbrebanov.selinux
-[ ! -d $rolesdir/kbrebanov.dkms ] && git clone https://github.com/kbrebanov/ansible-dkms.git $rolesdir/kbrebanov.dkms
-[ ! -d $rolesdir/kbrebanov.kernel_headers ] && git clone https://github.com/kbrebanov/ansible-kernel_headers.git $rolesdir/kbrebanov.kernel_headers
-[ ! -d $rolesdir/kbrebanov.virtualbox ] && git clone https://github.com/kbrebanov/ansible-virtualbox.git $rolesdir/kbrebanov.virtualbox
+#[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+## galaxy naming: kitchen fails to transfer symlink folder
+#[ ! -e $rolesdir/juju4.volatility ] && ln -s ansible-volatility $rolesdir/juju4.volatility
+[ ! -e $rolesdir/juju4.volatility ] && cp -R $rolesdir/ansible-volatility $rolesdir/juju4.volatility
+
+## don't stop build on this script return code
+true
 
