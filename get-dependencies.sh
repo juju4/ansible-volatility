aab9a0478166c9ed0cf0763ef062789ce647b45a
juju4 <juju4@users.noreply.github.com>
2017-04-30 09:57:58 -0400

    use system python-imaging (requires epel on rhel/centos) instead of direct pillow or pil

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,7 +9,7 @@ else
 rolesdir=$(dirname $0)/..
 fi
 
-#[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.volatility ] && ln -s ansible-volatility $rolesdir/juju4.volatility
 [ ! -e $rolesdir/juju4.volatility ] && cp -R $rolesdir/ansible-volatility $rolesdir/juju4.volatility
