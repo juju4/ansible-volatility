b4816d405f0e02a9f2615b35054b450af76a4a33
juju4 <juju4@users.noreply.github.com>
2020-05-10 15:02:40 -0400

    test/default*: update serverspec tests

--- test/integration/default-scan/serverspec/volatility_spec.rb
+++ test/integration/default-scan/serverspec/volatility_spec.rb
@@ -16,12 +16,18 @@ describe file('/usr/bin/vol.py'), :if => os[:family] == 'redhat' do
   it { should be_executable }
 end
 
-describe file('/usr/local/share/volatility/profiles') do
+describe file('/usr/local/share/volatility/profiles'), :if => os[:release] != '20.04' do
   it { should be_directory }
 end
 
-describe command('vol.py -h') do
+describe command('vol.py -h'), :if => os[:release] != '20.04' do
   its(:stdout) { should match /Usage: Volatility/ }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('vol.py -h'), :if => os[:release] == '20.04' do
+  its(:stdout) { should match /Volatility 3 Framework/ }
+  its(:stdout) { should match /usage: volatility / }
   its(:exit_status) { should eq 0 }
 end
 
--- test/integration/default-v3/serverspec/volatility_spec.rb
+++ test/integration/default-v3/serverspec/volatility_spec.rb
@@ -16,12 +16,9 @@ describe file('/usr/bin/vol.py'), :if => os[:family] == 'redhat' do
   it { should be_executable }
 end
 
-describe file('/usr/local/share/volatility/profiles') do
-  it { should be_directory }
-end
-
 describe command('vol.py -h') do
-  its(:stdout) { should match /Usage: Volatility/ }
+  its(:stdout) { should match /Volatility 3 Framework/ }
+  its(:stdout) { should match /usage: volatility / }
   its(:exit_status) { should eq 0 }
 end
 
